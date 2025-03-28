504a1827104866cf4bf87feaac025b6a618daca1
iuri lima <audazzk@gmail.com>
2025-03-28 16:16:15 -0300

    Exception handling - findById

--- src/main/java/com/iuritest/course/services/UserService.java
+++ src/main/java/com/iuritest/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.iuritest.course.entities.User;
 import com.iuritest.course.repositories.UserRepository;
+import com.iuritest.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
